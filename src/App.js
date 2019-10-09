f9a04b3efd98199b15c15db2290700f6963c4c57
Joe <josephc.lambert@gmail.com>
2019-10-09 15:44:21 -0700

    fixes render bug in additional features

--- src/App.js
+++ src/App.js
@@ -11,16 +11,15 @@ const App = () => {
     car: {
       price: 26395,
       name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
+      image: 'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: [],
     },
     additionalFeatures: [
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
       { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
+      { id: 4, name: 'Rear spoiler', price: 250 },
+    ],
   };
 
   const removeFeature = item => {
@@ -32,13 +31,13 @@ const App = () => {
   };
 
   return (
-    <div className="boxes">
-      <div className="box">
+    <div className='boxes'>
+      <div className='box'>
         <Header car={state.car} />
         <AddedFeatures car={state.car} />
       </div>
-      <div className="box">
-        <AdditionalFeatures store={state.store} />
+      <div className='box'>
+        <AdditionalFeatures store={state.additionalFeatures} />
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
