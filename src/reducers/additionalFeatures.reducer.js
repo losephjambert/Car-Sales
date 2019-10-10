cad3e2c9219d030f18357103017d0fd862eeda66
Joe <josephc.lambert@gmail.com>
2019-10-10 08:47:55 -0700

    removes console logs

--- src/reducers/additionalFeatures.reducer.js
+++ src/reducers/additionalFeatures.reducer.js
@@ -4,7 +4,6 @@ import { ADD_FEATURE, REMOVE_FEATURE } from '../actions';
 export default (state = initialState.additionalFeatures, action) => {
   switch (action.type) {
     case ADD_FEATURE:
-      console.log('add feature', state, action.payload);
       return [...state.filter(f => f.id !== action.payload.id)];
     case REMOVE_FEATURE:
       const sortedState = [...state, { ...action.payload }].sort((a, b) => a.id - b.id);
