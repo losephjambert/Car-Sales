70d725178b63f7d312b50b8bed212b6d837d82cf
Joe <josephc.lambert@gmail.com>
2019-10-10 08:24:46 -0700

    handles all actions in reducers

--- src/reducers/car.reducer.js
+++ src/reducers/car.reducer.js
@@ -1,15 +1,19 @@
 import initialState from '../state';
-import { ADD_FEATURE } from '../actions';
+import { ADD_FEATURE, REMOVE_FEATURE } from '../actions';
 
 const { car } = initialState;
 
 export default (state = car, action) => {
   switch (action.type) {
     case ADD_FEATURE:
-      const features = [...state.features, { ...action.payload }];
       return {
         ...state,
-        features,
+        features: [...state.features, { ...action.payload }],
+      };
+    case REMOVE_FEATURE:
+      return {
+        ...state,
+        features: [...state.features.filter(f => f.id !== action.payload.id)],
       };
     default:
       return state;
