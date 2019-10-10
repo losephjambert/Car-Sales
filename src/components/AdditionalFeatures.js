a8327b0def38aaeb7d8ea73d86383319b7a40bbc
Joe <josephc.lambert@gmail.com>
2019-10-10 07:21:57 -0700

    hooks up additional features and Total to redux store.

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 
-const AdditionalFeatures = props => {
+const AdditionalFeatures = ({ additionalFeatures }) => {
   return (
-    <div className="content">
+    <div className='content'>
       <h4>Additional Features</h4>
-      {props.store.length ? (
-        <ol type="1">
-          {props.store.map(item => (
+      {additionalFeatures.length ? (
+        <ol type='1'>
+          {additionalFeatures.map(item => (
             <AdditionalFeature key={item.id} feature={item} />
           ))}
         </ol>
