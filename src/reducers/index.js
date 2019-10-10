ee9e6e7e2cddc2296b56fa6fd3282477422e9120
Joe <josephc.lambert@gmail.com>
2019-10-10 07:03:48 -0700

    adds additional features reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 import additionalPrice from './additionalPrice.reducer';
 import car from './car.reducer';
+import additionalFeatures from './additionalFeatures.reducer';
 
 const rootReducer = combineReducers({
   additionalPrice,
   car,
+  additionalFeatures,
 });
 
 const store = createStore(rootReducer);
