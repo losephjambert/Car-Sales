6251a021d0737834d9bec5440f93d86cf0750c0c
Joe <josephc.lambert@gmail.com>
2019-10-10 08:24:05 -0700

    renames file to more generic name.

--- src/actions/additionalFeatures.actions.js
+++
@@ -1,5 +0,0 @@
-export const ADD_FEATURE = 'ADD_FEATURE';
-export const addFeature = feature => {
-  console.log('add feature', feature);
-  return { type: ADD_FEATURE, payload: { ...feature } };
-};
