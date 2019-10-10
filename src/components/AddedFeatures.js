80352747699050cd7c80f89df8d4a3c9a1c4fd56
Joe <josephc.lambert@gmail.com>
2019-10-10 08:23:14 -0700

    implements remove feature functionality in components

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -10,7 +10,7 @@ const AddedFeatures = props => {
       {props.features.length ? (
         <ol type='1'>
           {props.features.map(item => (
-            <AddedFeature key={item.id} feature={item} onClick={props.test} />
+            <AddedFeature key={item.id} feature={item} onClick={props.test} removeFeature={props.removeFeature} />
           ))}
         </ol>
       ) : (
